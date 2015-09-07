9918f74f3d3b0f5f8165cbd78ff9103b234953b6
Maciej Sypień <maciejsypien@gmail.com>
2015-09-07 23:46:05 +0200

    update path to Gulpfile.coffee

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,2 @@
-// Note the new way of requesting CoffeeScript since 1.7.x
 require('coffee-script/register');
-// This bootstraps your Gulp's main file
-require('./Gulpfile.coffee');
+require('./gulp_src/Gulpfile.coffee');
