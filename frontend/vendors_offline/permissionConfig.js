26413db07dad3d836ff27638c3f6494cdadbe275
Bartosz Jakubowiak <bartosz.jakubowiak@itelo.pl>
2015-10-16 10:57:18 +0200

    reinstate permissionsConfig to git

---
+++ frontend/vendors_offline/permissionConfig.js
@@ -0,0 +1,1 @@
+common.permissionConfig = {"roles":["public","user","translator","programmer","manager","admin"],"accessLevels":{"public":"*","anon":["public"],"programmer":["programmer"],"user":["user","translator","programmer","manager","admin"],"translator-only":["translator"],"programmer-only":["programmer"],"management":["manager","admin"],"admin":["admin"]}};
